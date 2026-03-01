6e60934e4205449c0d03e4b318745f8fd15dfc1a
YvanoffP <petruzzi.yvan@gmail.com>
2026-03-01 14:34:32 +0100

    fix: escape errors

--- core/functions/frontmatter/tokens/read.py
+++ core/functions/frontmatter/tokens/read.py
@@ -10,4 +10,8 @@ def read_tokens(path: Union[str, Path]) -> int:
     readed = read_frontmatter(path, FM.tokens)
     if readed == {}:
         return 0
+    # if readed is not of type dict:
+    #     raise TypeError(f"Expected dict, got {type(readed)}")
+    if not isinstance(readed, dict):
+        return 0
     return readed.get(FM.tokens_key, 0)
